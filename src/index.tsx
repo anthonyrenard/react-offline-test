c21467aadf1d10b9a34245caa8ffce5b0f52ec06
Anthony Renard <anthony@drall.agency>
2023-08-01 01:13:12 +0200

    :bug: Migrate to react 18 new api to follow the doc

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './app';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render((<App />), document.getElementById('reactMountPoint'));
-});
+import { App } from "./app";
+
+const container = document.getElementById("reactMountPoint");
+const root = createRoot(container!);
+
+root.render(<App />);
