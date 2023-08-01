c9b3afce047a33bdaccad0eee8657a7b14fe1480
Anthony Renard <anthony@drall.agency>
2023-08-01 12:57:56 +0200

    :sparkles: Use components

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const App = () => <h1>UK Energy Mix</h1>;
+import { Header } from "./components/Header";
+import { RequestProvider } from "./providers";
+import { List, PieChart } from "./components";
 
-export {
-    App
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8">
+        <RequestProvider>
+          <List />
+          <PieChart />
+        </RequestProvider>
+      </main>
+    </>
+  );
+};
+
+export { App };
